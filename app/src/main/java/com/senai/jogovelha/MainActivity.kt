cda4143bbc5af57ac0b3efb09a9889316a238f85
21276377 <https://github.com/omarcus212/andoridstudio.git>
2022-03-15 16:36:41 -0300

    hora do jogo man

---
+++ app/src/main/java/com/senai/jogovelha/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.senai.jogovelha
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val oneplayer = findViewById<Button>(R.id.oneplayer)
+        oneplayer.setOnClickListener{chamanadoframeoneplayer(oneplayer)}
+
+
+            
+}
+
+    private fun chamanadoframeoneplayer(oneplayer : Button){
+       val segundaFrame = Intent(this, jogovelhaframe2::class.java)
+
+        startActivity(segundaFrame)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/senai/jogovelha/jogovelhaframe2.kt
@@ -0,0 +1,13 @@
+package com.senai.jogovelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class jogovelhaframe2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jogovelhaframe2)
+
+
+    }
+}
