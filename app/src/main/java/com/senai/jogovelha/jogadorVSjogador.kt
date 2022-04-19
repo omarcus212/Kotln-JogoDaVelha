b408f4405d61238ee362763d9ea39224536a1bdc
21276377 <https://github.com/omarcus212/andoridstudio.git>
2022-04-19 14:53:31 -0300

    .

--- app/src/main/java/com/senai/jogovelha/jogadorVSjogador.kt
+++ app/src/main/java/com/senai/jogovelha/jogadorVSjogador.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 
 
 var jogador = 1;
@@ -30,6 +31,7 @@ class jogadorVSjogador : AppCompatActivity() {
         val btnList = arrayListOf<Button>(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
 
 
+
         btnList.forEach {
             it.setOnClickListener { jogoclick(it as Button) }
         }
@@ -48,14 +50,14 @@ class jogadorVSjogador : AppCompatActivity() {
 
 
         val vezjogador = findViewById<TextView>(R.id.vez)
-        val result = findViewById<TextView>(R.id.result)
+
 
         if (jogador == 1) {
             vezjogador.text = "vez do jogador 2"
 
             btn.text = "x"
             btn.textSize = 80f
-            btn.isEnabled = false
+            btn.isClickable = false
             jogador = 2;
 
 
@@ -65,10 +67,14 @@ class jogadorVSjogador : AppCompatActivity() {
             btn.text = "o"
             btn.textSize = 80f
 
-            btn.isEnabled = false
+            btn.isClickable = false
             jogador = 1;
 
         }
+
+
+        val resete = findViewById<TextView>(R.id.reset)
+
 
 
 
@@ -86,43 +92,93 @@ class jogadorVSjogador : AppCompatActivity() {
 
         val btnList = arrayListOf<Button>(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
 
-        if (btnList[0].text == "x" && btnList[1].text == "x" && btnList[2].text == "x") {
-            ganhador()
-              endgame();
+        if (btnList[0].text == "x" && btnList[1].text == "x" && btnList[2].text == "x" || btnList[0].text == "x" && btnList[3].text == "x" && btnList[6].text == "x"){
+            ganhador();
+            endgame(btnList);
+            resete.isVisible = true;
+            resete.setOnClickListener { reset(btnList) }
+
+
+        }else if (btnList[3].text == "x" && btnList[4].text == "x" && btnList[5].text == "x" || btnList[1].text == "x" && btnList[5].text == "x" && btnList[8].text == "x"){
+            ganhador();
+            endgame(btnList);
+            resete.isVisible = true;
+            resete.setOnClickListener { reset(btnList) };
+
+
+        }else if(btnList[6].text == "x" && btnList[7].text == "x" && btnList[8].text == "x" || btnList[2].text == "x" && btnList[6].text == "x" && btnList[9].text == "x"){
+            ganhador();
+            endgame(btnList);
+            resete.isVisible = true;
+            resete.setOnClickListener { reset(btnList)
+             btnList[1].isClickable =true;
+            }
+
+        }else if (btnList[0].text == "x" && btnList[4].text == "x" && btnList[8].text == "x"){
+            ganhador();
+            endgame(btnList);
+            resete.isVisible = true;
+            resete.setOnClickListener { reset(btnList) }
+
+        }else if(btnList[2].text == "x" && btnList[4].text == "x" && btnList[6].text == "x"){
+
+            ganhador();
+            endgame(btnList);
+            resete.isVisible = true;
+            resete.setOnClickListener { reset(btnList) }
+
+        }else if(btnList[0].text == "o" && btnList[1].text == "o" && btnList[2].text == "o" || btnList[0].text == "o" && btnList[3].text == "o" && btnList[6].text == "o"){
+            ganhador();
+            endgame(btnList);
+            resete.isVisible = true;
+            resete.setOnClickListener { reset(btnList) }
         }
+
+
 
 
 
 
     }
 
-    private fun endgame() {
+    private fun endgame(bntlist : ArrayList<Button>) {
 
-        val btn1 = findViewById<Button>(R.id.btn1)
-        val btn2 = findViewById<Button>(R.id.btn2)
-        val btn3 = findViewById<Button>(R.id.btn3)
-        val btn4 = findViewById<Button>(R.id.btn4)
-        val btn5 = findViewById<Button>(R.id.btn5)
-        val btn6 = findViewById<Button>(R.id.btn6)
-        val btn7 = findViewById<Button>(R.id.btn7)
-        val btn8 = findViewById<Button>(R.id.btn8)
-        val btn9 = findViewById<Button>(R.id.btn9)
 
-        val btnList = arrayListOf<Button>(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
-
-        for(x in 0..btnList.size-1){
-            btnList[x].isClickable = false
+        for(x in 0..bntlist.size-1){
+            bntlist[x].isClickable = false
         }
     }
 
 
     private fun ganhador() {
         val result = findViewById<TextView>(R.id.result)
-        if (jogador % 2 != 0) {
+        if (jogador == 1) {
             result.text = "Ganhador: 0"
 
         } else {
             result.text = "Ganhador: x"
+        }
+
+    }
+
+    private fun empate(btnlist: ArrayList<Button>){
+        val result = findViewById<TextView>(R.id.result)
+        if(btnlist.size != 0){
+            result.text = "empate"
+        }
+
+    }
+
+    private  fun reset(btns:ArrayList<Button>){
+        val result = findViewById<TextView>(R.id.result)
+
+        jogador = 1;
+        result.text = "user : 0   VS   0 : user2";
+
+        btns.forEach {
+            it.text = ""
+            it.isClickable = true
+
         }
 
     }
